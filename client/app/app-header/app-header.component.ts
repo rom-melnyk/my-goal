e0d44f0c30400f2665f92304555d7b04c71aeb9a
Roman <email.rom.melnyk@gmail.com>
2018-05-10 22:02:46 +0200

    Nav moved to header

--- client/app/app-header/app-header.component.ts
+++ client/app/app-header/app-header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { StaticDataService } from '../services/static-data/static-data.service';
+import { Router, Event, NavigationEnd, } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import { parse as urlParse } from 'url';
 
 
 @Component({
@@ -10,14 +14,22 @@ import { StaticDataService } from '../services/static-data/static-data.service';
 export class AppHeaderComponent implements OnInit {
   title: string;
   slogan: string;
+  currentLocation: string = '';
 
   constructor(
-    private staticData: StaticDataService
+    private staticData: StaticDataService,
+    private router: Router,
   ) { }
 
   ngOnInit() {
     this.title = this.staticData.title;
     this.slogan = this.staticData.slogan;
+    this.router.events
+      .filter((e: Event) => e instanceof NavigationEnd)
+      .map((event: NavigationEnd) => urlParse(event.url).pathname)
+      .subscribe((path: string) => {
+        this.currentLocation = path;
+      });
   }
 
 }
